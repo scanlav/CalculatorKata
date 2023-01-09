b86b956d8f8d259ebfac9e0ff59088728d8fc078
Viktor Churikov <51499715+scanlav@users.noreply.github.com>
2023-01-09 23:48:55 +0300

    Correction of the test task
    Making changes in accordance with the requirements of the mentor

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,47 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        String help = """
-                Калькулятор принимает данные в формате "a + b"
-                Поддерживаются стандартные арифметические операции (+, -, *, /), а так же целые арабские цифры (1 - 10)
-                Калькулятор не принимает римские цифры, строки, или иные операции, не предусмотренные инструкцией. На ноль делить нельзя.
-                Для получения справки введите "help"
-                Для выхода из приложения введите "exit"
-                """;
+//        System.out.println(calc("1 # 2"));
 
-        System.out.println(help);
-        Scanner in = new Scanner(System.in);
+        System.out.println(calc("1 + 2"));
+        System.out.println(calc("1 / 5"));
+        System.out.println(calc("5 - 1"));
+        System.out.println(calc("2 * 9"));
 
+//        System.out.println(calc("20 + 4"));
 
+//        System.out.println(calc("1"));
+
+//        System.out.println(calc("1 + 2 + 3"));
+
+    }
+    public static String calc(String input) {
+        int result = 0;
         try {
-            while (true) {
-                System.out.println("Введите данные для вычисления: ");
-                String input = in.nextLine();
-                if (input.equalsIgnoreCase("help")) {
-                    System.out.println(help);
-                } else if (input.equalsIgnoreCase("exit")) {
-                    break;
-                } else {
-                    String[] input_data = input.split(" ");
 
-                    int a = Integer.parseInt(input_data[0]);
-                    int b = Integer.parseInt(input_data[2]);
-                    String arithmetic_operation = input_data[1];
+            String[] input_data = input.split(" ");
 
-                    if ((a >= 1 && a <= 10) && (b >= 1 && b <= 10)) {
-                        switch (arithmetic_operation) {
-                            case "+" -> System.out.println(a + b);
-                            case "-" -> System.out.println(a - b);
-                            case "*" -> System.out.println(a * b);
-                            case "/" -> System.out.println(a / b);
-                            default -> throw new Exception();
-                        }
-                    } else {
-                        throw new Exception();
-                    }
+            int a = Integer.parseInt(input_data[0]);
+            int b = Integer.parseInt(input_data[2]);
+            String arithmetic_operation = input_data[1];
+            if (input_data.length >= 4) {
+                throw new Exception();
+            } else if ((a >= 1 && a <= 10) && (b >= 1 && b <= 10)) {
+                switch (arithmetic_operation) {
+                    case "+" -> result = a + b;
+                    case "-" -> result = a - b;
+                    case "*" -> result = a * b;
+                    case "/" -> result = a / b;
+                    default -> throw new Exception();
                 }
+            } else {
+                throw new Exception();
             }
         } catch (Exception exception) {
-            System.out.println("Введенные данные не верны. Внимательно читайте инструкцию.");
+            return "throws Exception";
         }
+        return Integer.toString(result);
     }
 }
